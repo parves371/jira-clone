16f6220dc7ba97ee218c12609965192a3e87b298
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-11-16 16:59:49 +0600

    Setting up Appwrite & database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button> Click me  </Button>
+      <Button> Click me </Button>
       <Button variant="destructive"> destructive </Button>
-      <Button variant="outline"> outline  </Button>
-      <Button variant="secondary"> secondary  </Button>
+      <Button variant="outline"> outline </Button>
+      <Button variant="secondary"> secondary </Button>
       <Button variant="ghost"> ghost </Button>
-      <Button variant="muted"> muted  </Button>
+      <Button variant="muted"> muted </Button>
       <Button variant="teritary"> teritary </Button>
     </div>
   );
---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "pas-jira-clone-session";
